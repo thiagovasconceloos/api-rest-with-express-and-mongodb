95c10f86cb2f4a2390b904a435f5de62378395ea
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-05-08 19:27:04 -0300

    Método GET

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
-import db from "../config/dbConnect.js";
-import livros from "./models/Livro.js"
+import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
+import routes from "./routes/index.js"
 
 
 db.on("error",console.log.bind(console,'Erro de Conexão'))
@@ -14,23 +15,8 @@ const app = express();
 
 app.use(express.json())
 
-
-// const livros = [
-//   {id: 1, "titulo": "Senhor dos Aneis"},
-//   {id: 2, "titulo": "O Hobbit"}
-// ]
-
-app.get('/', (req, res) => {
-  res.status(200).send('Curso de Node');
-}) 
-
-app.get('/livros', (req, res) => {
-  livros.find((err,livros)=>{
-    res.status(200).json(livros);
-
-  })
+routes(app)
  
-})
 
 app.get('/livros/:id', (req, res) => {
   let index = buscaLivro(req.params.id);
---
+++ src/controllers/livrosController.js
@@ -0,0 +1,18 @@
+import livros from "../models/Livro.js";
+
+class livroController{
+
+static listarLivros=(req,res) =>{
+
+    livros.find((err,livros)=>{
+    res.status(200).json(livros);
+})
+
+
+
+
+}
+}
+
+
+export default livroController;
