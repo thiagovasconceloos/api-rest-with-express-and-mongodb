d7b8c8fbf91b004f1df2577f0e203d09ad19143f
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-05-08 16:30:51 -0300

    Evoluindo as rotas

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
+
 const livros = [
   {id: 1, "titulo": "Senhor dos Aneis"},
   {id: 2, "titulo": "O Hobbit"}
@@ -12,7 +15,35 @@ app.get('/', (req, res) => {
 }) 
 
 app.get('/livros', (req, res) => {
-  res.status(200).json(livros)
+  res.status(200).json(livros);
 })
+
+app.get('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  res.json(livros[index]);
+})
+
+
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro foi cadastrado com sucesso');
+  })
+
+  app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+  })
+
+
+ function buscaLivro(id){
+
+
+   return livros.findIndex(livro => livro.id == id);
+
+
+ }
+
 
 export default app
