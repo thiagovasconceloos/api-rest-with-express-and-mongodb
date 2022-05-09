f2df75662d3c6569b3884086cf4dc1083634d590
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-05-08 22:07:28 -0300

    Acrescentando um novo Schema

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import livros from "./livrosRoutes.js"
+import autores from "./autoresRoutes.js"
 
 const  routes = (app) =>{
  
@@ -12,7 +13,8 @@ const  routes = (app) =>{
  app.use(
 
   express.json(),
-  livros
+  livros,
+  autores
 
  )
  
